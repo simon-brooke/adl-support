dc1b4a5e44d70b0ec79ca5ea88f33ff3ad2ed9c5
Simon Brooke <simon@journeyman.cc>
2018-07-19 21:52:16 +0100

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,9 +1,25 @@
-(defproject adl-support "0.1.1-SNAPSHOT"
+(defproject adl-support "0.1.1"
   :description "A small library of functions called by generated ADL code."
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [selmer "1.10.6"]])
+                 [selmer "1.10.6"]]
+
+  :plugins [[lein-codox "0.10.3"]
+            [lein-release "1.0.5"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ;; ["vcs" "tag"] -- not working, problems with secret key
+                  ["clean"]
+                  ["test"]
+                  ["uberjar"]
+                  ["codox"]
+                  ["install"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]])
