c9a6a623d03e7572cb6891a6af8982325e70aece
Simon Brooke <simon@journeyman.cc>
2018-09-22 09:22:12 +0100

    lein-release plugin: preparing 0.1.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adl-support "0.1.5-SNAPSHOT"
+(defproject adl-support "0.1.5"
   :description "A small library of functions called by generated ADL code."
   :url "https://github.com/simon-brooke/adl-support"
   :license {:name "MIT License"
