ef00e3b043cdf01284630733849b6ab885eaea47
Simon Brooke <simon@journeyman.cc>
2025-05-20 09:45:40 +0100

    Upversioned to 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adl-support "0.1.6"
+(defproject adl-support "0.1.7"
   :description "A small library of functions called by generated ADL code."
   :url "https://github.com/simon-brooke/adl-support"
   :license {:name "MIT License"
